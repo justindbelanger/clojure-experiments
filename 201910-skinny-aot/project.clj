9b19b27a01b1d0ad527ae18d098afd4d8266084d
Justin Belanger <justindbelanger@outlook.com>
2019-10-28 22:00:01 -0700

    Demonstrates problem with skinny JAR
    - uses lein to produce skinny JAR
    - propertied library consistently
      blows up JAR--regardless of
      what built the code

--- 201910-skinny-aot/project.clj
+++ 201910-skinny-aot/project.clj
@@ -1,6 +1,13 @@
 (defproject skinny-jar "1.0.0.rc1"
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [com.taoensso/timbre "4.10.0" ]]
+  ;; :dependencies [[clojurewerkz/propertied "1.3.0"]
+  ;;                [com.taoensso/timbre "4.10.0" ]
+  ;;                [org.clojure/clojure "1.10.1"]]
+
+  :plugins [[lein-tools-deps "0.4.5"]
+            [lein-aot-filter "0.1.0"]]
+  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
+  :lein-tools-deps/config {:config-files [:install :project]}
+  :aot-include [#"rgm.*"]
 
   :jar-name "skinny-lein.jar"
   :source-paths ["src"]
--- 201910-skinny-aot/src/rgm/core.clj
+++ 201910-skinny-aot/src/rgm/core.clj
@@ -1,7 +1,14 @@
 (ns rgm.core
   (:gen-class)
-  (:require [taoensso.timbre :as timbre]))
+  (:require [clojure.java.io :as io]
+            [clojurewerkz.propertied.properties :as p]
+            [taoensso.timbre :as timbre]))
 
 (defn -main
   []
-  (timbre/info "yep, we booted right, from a skinny jar"))
+  (timbre/info "yep, we booted right, from a skinny jar")
+  (timbre/info "Here's the system.properties file:"
+               (-> "system.properties"
+                   io/file
+                   p/load-from
+                   p/properties->map)))
