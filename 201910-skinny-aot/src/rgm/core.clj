1a6ecb79dbbb0e521860a7f6c5a9c0eeb364e7f5
Justin Belanger <justindbelanger@outlook.com>
2019-10-30 15:58:34 -0700

    Demonstrate AOT problem with reitit

--- 201910-skinny-aot/src/rgm/core.clj
+++ 201910-skinny-aot/src/rgm/core.clj
@@ -1,14 +1,21 @@
 (ns rgm.core
   (:gen-class)
   (:require [clojure.java.io :as io]
-            [clojurewerkz.propertied.properties :as p]
+            ;; [clojurewerkz.propertied.properties :as p]
+            [reitit.core :as rc]
             [taoensso.timbre :as timbre]))
+
+(def router
+  (rc/router
+   [["/api/ping" ::ping]
+    ["/api/orders/:id" ::order]]))
 
 (defn -main
   []
   (timbre/info "yep, we booted right, from a skinny jar")
-  (timbre/info "Here's the system.properties file:"
-               (-> "system.properties"
-                   io/file
-                   p/load-from
-                   p/properties->map)))
+  (timbre/info (rc/match-by-name router "/api/ping"))
+  #_(timbre/info "Here's the system.properties file:"
+                 (-> "system.properties"
+                     io/file
+                     p/load-from
+                     p/properties->map)))
